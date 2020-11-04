f4b4c1c5e2857650651e27feeba9158ee332ce87
asharma984 <aditya.sharma984@gmail.com>
2020-11-04 00:39:38 -0500

    sliding image

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
-
+import { Zoom } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 class Pet extends Component{
     constructor(props){
@@ -14,16 +15,26 @@ class Pet extends Component{
 
         return (
 
-            <div className="ui card" style={{ background: "#007bff"}}>
-                <section>
+        <div  style={{ background: "#EAE7DC"}} >
+                {/*<section>*/}
 
-                    <img src={this.state.animal.primary_photo_cropped.medium} />
-                </section>
+                {/*    <img src={this.state.animal.primary_photo_cropped.medium}  />*/}
+                {/*</section>*/}
+            <div className="slide-container " style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <Zoom scale={0.2} duration={500} >
+                    {
+                        this.state.animal.photos.map((each, index) => <img key={index} style={{width: "20%"}} src={each.small} />)
+                    }
+                </Zoom>
+            </div>
                 {/*<h1>{this.state.animal.name}</h1>*/}
-               <h2>Age: {this.state.animal.age}</h2>
-                <h2> Description:{this.state.animal.description}</h2>
-                <h2>Gender:{this.state.animal.gender}</h2>
-                <h2>Species: {this.state.animal.species}</h2>
+               <h2 style={{ padding: "10px 20px", color: "#AC3B61"}}>Age: {this.state.animal.age}</h2>
+                <h2 style={{ padding: "10px 20px", color: "#123C69"}}> Description:{this.state.animal.description}</h2>
+                <h2 style={{ padding: "10px 20px", color: "#AC3B61"}}>Gender:{this.state.animal.gender}</h2>
+                <h2 style={{ padding: "10px 20px", color: "#123C69"}}>Species: {this.state.animal.species}</h2>
                 <a href={this.state.animal.url}><h2>View full profile</h2></a>
 
             </div>
